0d151d6028b3b22db5e172d6df4d8b06bb28a458
tang <eytang8@gmail.com>
2019-09-02 20:04:12 -0700

    added exports

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const fetch = require('node-fetch');
-function isPromise(value) {
+export function isPromise(value) {
   return Boolean(value && typeof value.then === 'function');
 }
 
-function timePropmise(prom) {
-  const start = Date.now();
-  return prom.then(val => {
-    const time = Date.now() - start;
-  });
-}
-
-function wrapFunction(fn) {
-  const prom = fn();
-  if (isPromise(prom)) {
-    return timePromise(prom);
-  }
-}
-
 //invokes and times functions (works for both promises and sync functions)
-function timePromOrVal(fn) {
-  const start = Date.now();
-  const prom = fn();
+export function timePromOrVal(returnedVal, start = Date.now()) {
   let result;
-  if (!isPromise(prom)) {
+  if (!isPromise(returnedVal)) {
     result = new Promise((resolve, reject) => {
-      resolve(prom);
+      resolve(returnedVal);
     });
   } else {
-    result = prom;
+    result = returnedVal;
   }
   return result.then(val => {
     return {
@@ -37,18 +21,12 @@ function timePromOrVal(fn) {
   });
 }
 
-// testing promise and non-promise vals
+//bringing it all together
+// returns a promise that will resolve with its val and elapsed time
 
-const fetchThing = () => {
-  return fetch('https://api.github.com/users/github');
-};
-
-const syncOp = () => 5;
-
-timePromOrVal(fetchThing).then(data => {
-  console.log(data);
-});
-
-timePromOrVal(syncOp).then(data => {
-  console.log(data);
-});
+export function timeIt(fn) {
+  return function(...args) {
+    const now = Date.now();
+    return timePromOrVal(fn(...args), now);
+  };
+}
